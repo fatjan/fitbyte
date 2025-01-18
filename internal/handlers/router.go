530a1f9a3f1a945389ace74409322bb1118510c9
Ainur Rofiq <rizqullohbrilliant@protonmail.com>
2025-01-18 19:22:53 +0000

    feat: post activity

--- internal/handlers/router.go
+++ internal/handlers/router.go
@@ -3,8 +3,10 @@ package handlers
 import (
 	"github.com/fatjan/fitbyte/internal/config"
 	"github.com/fatjan/fitbyte/internal/pkg/jwt_helper"
+	activityRepository "github.com/fatjan/fitbyte/internal/repositories/activity"
 	authRepository "github.com/fatjan/fitbyte/internal/repositories/auth"
 	duckRepo "github.com/fatjan/fitbyte/internal/repositories/duck"
+	activityUseCase "github.com/fatjan/fitbyte/internal/usecases/activity"
 	authUseCase "github.com/fatjan/fitbyte/internal/usecases/auth"
 	duckUsecase "github.com/fatjan/fitbyte/internal/usecases/duck"
 	"github.com/gin-gonic/gin"
@@ -29,7 +31,15 @@ func SetupRouter(cfg *config.Config, db *sqlx.DB, r *gin.Engine) {
 	authUseCase := authUseCase.NewUseCase(authRepository, cfg)
 	authHandler := NewAuthHandler(authUseCase)
 
-	authRouter := v1.Group("auth")
+	authRouter := v1.Group("")
 	authRouter.POST("/register", authHandler.Register)
 	authRouter.POST("/login", authHandler.Login)
+
+	activityRepository := activityRepository.NewActivityRepository(db)
+	activityUseCase := activityUseCase.NewUseCase(activityRepository)
+	activityHandler := NewActivityHandler(activityUseCase)
+
+	activityRouter := v1.Group("activity")
+	activityRouter.Use(jwtMiddleware)
+	activityRouter.POST("/", activityHandler.Post)
 }
